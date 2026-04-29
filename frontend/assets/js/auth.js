ad93ee08fb63788d75f9c49b6211c0f9a954e531
Syaloom Pardede <s.pardede@tu-braunschweig.de>
2026-04-29 18:53:58 +0200

    feat: add auth.js and open close functions to see login modal popup

---
+++ frontend/assets/js/auth.js
@@ -0,0 +1,9 @@
+/*Modal pop-up */
+
+function openLoginModal() {
+  document.getElementById("loginModal").classList.add("active");
+}
+
+function closeLoginModal() {
+  document.getElementById("loginModal").classList.remove("active");
+}
