ba5927c570500955140e6d8c253c38be76f81f26
Fernando Amro <f.amro@tu-braunschweig.de>
2026-05-08 01:47:04 +0200

    feat: add an API to the Database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 //Start Server
 const app = require("./src/app");
 const PORT = 3000; //typically for Node-Servers
-app.listen(PORT, () => {
-  console.log(`✅ Server läuft auf http://localhost:${PORT}`);
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log("The Server did not start:", err);
+    return;
+  }
+  console.log(`The Server is running on http://localhost:${PORT}`);
 });
