c6d81be45c873a3ee98dab4cbed7a17dae690316
fira <n.binti-rosli@tu-braunschweig.de>
2026-06-07 00:07:34 +0200

    feat: add adbreak.js

---
+++ backend/src/routes/adbreak.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const songsAdBreak = require("../services/songsadbreak");
+const router = express.Router();
+
+router.post ("/submitAdText", express.json(), (req, res) => {
+    const { username, adBreakText } = req.body;
+
+    //Save the submitted ad break text to the database. 
+    songsAdBreak.addAdBreak(username, adBreakText);
+    res.status(200).json({ message: "Ad break text is now waiting for approval from admin." });
+
+});
+module.exports = router;
