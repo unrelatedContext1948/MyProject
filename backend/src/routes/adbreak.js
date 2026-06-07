15bf95e426b7d82a35c4ea16ba58ecacb59b4ea2
fira <n.binti-rosli@tu-braunschweig.de>
2026-06-07 16:12:24 +0200

    fix: the message

--- backend/src/routes/adbreak.js
+++ backend/src/routes/adbreak.js
@@ -2,12 +2,14 @@ const express = require("express");
 const songsAdBreak = require("../services/songsadbreak");
 const router = express.Router();
 
-router.post ("/submitAdText", express.json(), (req, res) => {
+router.post("/submitAdText", express.json(), (req, res) => {
     const { username, adBreakText } = req.body;
 
     //Save the submitted ad break text to the database. 
     songsAdBreak.addAdBreak(username, adBreakText);
-    res.status(200).json({ message: "Ad break text is now waiting for approval from admin." });
+    res.status(200).json({
+         message: "Ad break text is now waiting for approval from admin." 
+        });
 
 });
 module.exports = router;
