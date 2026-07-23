81372c19782ccea9e1519fd00533bdbfef8e83f9
Fernando Amro <f.amro@tu-braunschweig.de>
2026-07-24 00:36:49 +0200

    feat: new Table was added to the database for counting likes

--- backend/src/database/database.js
+++ backend/src/database/database.js
@@ -2,6 +2,7 @@ const Database = require("better-sqlite3");
 const path = require("path");
 const dbPath = path.join(__dirname, "database.db");
 const db = new Database(dbPath);
+db.pragma("foreign_keys = ON");
 
 db.exec(`
 CREATE TABLE IF NOT EXISTS UsersTable(
@@ -31,6 +32,19 @@ CREATE TABLE IF NOT EXISTS AdBreaksTable(
     AdBreakText TEXT NOT NULL,
     AdBreakURL TEXT NOT NULL,
     Status TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS LikesTable(
+    LikesID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    UserID INTEGER NOT NULL,
+    PlaylistID INTEGER NOT NULL,
+    UNIQUE (UserID, PlaylistID),
+    FOREIGN KEY (UserID) REFERENCES UsersTable(UserID)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    FOREIGN KEY (PlaylistID) REFERENCES PlaylistsTable(PlaylistID)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
 
 `);
